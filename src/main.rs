48f77afcbdd7cd3b54884b90c273906801b85fc3
0DoubleDare <lidernikita905@gmail.com>
2026-02-05 16:42:20 +0400

    Minimum working version. Added: - Display download progress - More debug information in the console - Optimization and bug fixes
    Attention! During downloading, if yt-dlp does not respond after 30> seconds, then it crashes and stops downloading videos. This usually happens with videos that exceed 15 minutes in length.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@
 use eframe::egui;
 use std::{ path::PathBuf, error::Error };
 use rfd::FileDialog;
-use yt_dlp::prelude::{VideoQuality, VideoCodecPreference};
+use yt_dlp::prelude::{VideoQuality, VideoCodecPreference, ManagerConfig};
 use yt_dlp::{Youtube, client::Libraries, model::AudioQuality};
 use std::sync::mpsc::{Sender, Receiver, channel };
+use std::process::Command;
+use std::path::Path;
+use std::time::Duration;
+use egui::FocusDirection::Down;
+use tokio::sync::watch;
+use yt_dlp::model::format::DownloaderOptions;
 // use std::sync::OnceLock;
-
 /// Расширение исполняемого файла. Зависит от операционной системы.
 ///
 /// Windows - ".exe"
@@ -21,7 +26,7 @@ struct MyApp {
     text_url: String,
     file_path: Option<PathBuf>,
     download_progress: f32,
-    rx: Option<Receiver<f32>>,
+    rx: Option<watch::Receiver<f32>>,
 }
 
 impl Default for MyApp {
@@ -39,10 +44,8 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if let Some(ref rx) = self.rx {
-                while let Ok(val) = rx.try_recv() {
-                    self.download_progress = val;
-                    ctx.request_repaint();
-                }
+                self.download_progress = *rx.borrow();
+                ctx.request_repaint();
             }
             ui.heading("Video downloader");
 
@@ -58,25 +61,27 @@ impl eframe::App for MyApp {
             if ui.button("Start download").clicked() {
                 let url = self.text_url.clone();
                 let path = self.file_path.clone().expect("Select path in wrong");
-                let (tx, rx) = channel::<f32>();
+                let (tx, rx) =
+                    watch::channel(0.0f32);
 
                 self.rx = Some(rx);
 
                 tokio::spawn(async move {
-                    if let Err(e) = download_video(path, url).await {
+                    if let Err(e) = download_video(path, url, tx).await {
                         eprintln!("ERROR: {e}");
                     }
                 });
             }
 
-            ui.add(egui::ProgressBar::new(self.download_progress)
+            ui.
+                add(egui::ProgressBar::new(self.download_progress)
+                    .animate(true)
                 .show_percentage())
         });
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), eframe::Error> {
+fn main() -> Result<(), eframe::Error> {
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
@@ -103,9 +108,10 @@ async fn main() -> Result<(), eframe::Error> {
 pub async fn download_video(
     download_directory: PathBuf,
     video_url: String,
+    tx: watch::Sender<f32>
 ) -> Result<(), Box<dyn Error>> {
     let libraries_dir = PathBuf::from("libs");
-    let output_dir = PathBuf::from("output");
+    let output_dir = PathBuf::from(&download_directory);
 
     let youtube = libraries_dir.join("yt-dlp");
     let ffmpeg = libraries_dir.join("ffmpeg");
@@ -121,70 +127,34 @@ pub async fn download_video(
 
     let libraries = Libraries::new(youtube, ffmpeg);
     let fetcher = Youtube::new(libraries, output_dir).await?;
-    let video = fetcher.fetch_video_infos(video_url).await?;
 
+    let video = fetcher.fetch_video_infos(video_url.clone()).await?;
+
+    for format in &video.formats {
+        println!("{:?}", format);
+    }
     println!("video infos: {}", video);
 
-    let download_id = fetcher.download_video_with_progress(
-        &video,
-        "video-with-progress.mp4",
-        |downloaded, total| {
-            let percentage = if total > 0 {
-                (downloaded as f32 / total as f32 * 100.0) as u64
-            } else {
-                0
-            };
-            println!("Progress: {}/{} bytes ({}%)", downloaded, total, percentage);
-        }
-    ).await?;
+    let video_title = video.title;
+    let video_ext = "mp4".to_string();
+    let tx_progress = tx.clone();
 
-    fetcher.wait_for_download(download_id).await;
+    let download_id = fetcher.download(video_url, format!("{video_title}.{video_ext}"))
+        .video_quality(VideoQuality::Best)
+        .video_codec(VideoCodecPreference::AV1)
+        .audio_quality(AudioQuality::Best)
+        .with_progress(move |progress| {
+            let val = progress as f32;
+            println!("progress: {}%", (val * 100.0).round());
+            let _ = tx_progress.send(val).ok();
+        })
+        .execute()
+        .await?;
 
-    println!("Download complete");
-
+    println!("Download complete: video available in {:?}", download_directory);
+    let _ = tx.send(1.0).ok();
     Ok(())
 }
-// async fn download_video(
-//     output_dir: PathBuf,
-//     url: String,
-//     tx: Sender<f32>
-// ) -> Result<(), Box<dyn Error>> {
-//     std::fs::create_dir_all(&output_dir)?;
-//     #[cfg(windows)]
-//     let extension = ".exe";
-//     #[cfg(not(windows))]
-//     let extension = "";
-//
-//     let libraries_dir = PathBuf::from("libs");
-//     let youtube = libraries_dir.join(format!("yt-dlp{}", extension));
-//     let ffmpeg = libraries_dir.join(format!("ffmpeg{}", extension));
-//
-//     if !youtube.exists() || !ffmpeg.exists() {
-//         println!("Needed libraries not found. Installing...");
-//         let _ = download_library();
-//         println!("Needed libraries has been install");
-//     }
-//
-//     println!("yt-dlp path: {:?}; Exists: {}", youtube, youtube.exists());
-//     println!("ffmpeg path: {:?}; Exists: {}", ffmpeg, ffmpeg.exists());
-//
-//     let libraries = Libraries::new(youtube, ffmpeg);
-//     let fetcher = Youtube::new(libraries, output_dir.clone()).await.expect("Fetcher error");
-//
-//     println!("...Start download...");
-//     let _ = tx.send(0.5);
-//
-//     let video_path =
-//         fetcher.download(url.clone(), PathBuf::from("my-video.mp4"))
-//             .video_quality(VideoQuality::Best)
-//             .video_codec(VideoCodecPreference::AV1)
-//             .audio_quality(AudioQuality::Best)
-//             .execute().await?;
-//
-//     let _ = tx.send(1.0);
-//     println!("Downloaded to: {:?}", video_path);
-//     Ok(())
-// }
 
 pub async fn download_library() -> Result<(), Box<dyn Error>> {
     let executables_dir = PathBuf::from("libs");
